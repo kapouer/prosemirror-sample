ebbeafba2e17cf2e8c7b73fd1786390e496d270c
Jérémy Lal <kapouer@melix.org>
2017-12-17 17:40:32 +0100

    Commit old stuff

--- example1/main.js
+++ example1/main.js
@@ -7,22 +7,45 @@ var marks = require("prosemirror-schema-basic").marks
 nodes.testlist = {
 	content: "testitem*",
 	group: "block",
-	parseDOM: [{tag: '[block-type="list"]'}],
+	parseDOM: [{tag: 'div.list'}],
 	toDOM: function(node) {
-		return ["div", {'block-type': 'list'}, 0]
+		return ["div", {'class': 'list'}, 0]
 	}
 }
 
 nodes.testitem = {
 	isLeaf: true,
-	parseDOM: [{tag: '[block-type="item"]'}],
+//	view: function(node, view, getPos, decorations) {
+//		console.log("view item")
+//		var dom = document.createElement('div')
+//		dom.className = 'item'
+//		dom.textContent = 'X'
+//		return {
+//			dom: dom,
+//			update: function(node, decorations) { return true },
+//			ignoreMutation: function(record) { return true }
+//		}
+//	},
+	parseDOM: [{tag: 'div.item'}],
 	toDOM: function(node) {
-		return ["div", {'block-type': 'item'}]
+		return ["div", {'class': 'item'}]
 	}
 }
+
+
+
+var nodeViews = {}
+Object.keys(nodes).forEach(function(name) {
+	var node = nodes[name]
+	if (node.view) nodeViews[name] = node.view
+})
+
 document.addEventListener('DOMContentLoaded', function() {
 	new EditorView(document.body, {
-		state: EditorState.create({schema: new Schema({nodes, marks})}),
+		state: EditorState.create({
+			schema: new Schema({nodes, marks})
+		}),
+		nodeViews: nodeViews
 	})
 })
 
